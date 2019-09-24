0b6ff356642cded081ec313099027842c3d8ef92
crimsonprince <kingr4@protonmail.ch>
2019-09-24 15:27:39 +0100

    Added User & Society Model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+class User(AbstractUser):
+    age = models.CharField(4)
+    society = models.ManyToManyField('Society')
+
+    def __str__(self):
+        return self.email
+
+class Society():
+    name = models.CharField(255)
+    email = models.EmailField(255)
+    creation_date = models.DateField()
+    description = models.CharField()
+    category = models.CharField(255)
+    members = models.ManyToManyField('User')
