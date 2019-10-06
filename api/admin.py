a5f1c72ee4704e484c16e6a1b5cf59a0876d96da
crimsonprince <kingr4@protonmail.ch>
2019-10-06 15:40:23 +0100

    Altered Serialiation

--- api/admin.py
+++ api/admin.py
@@ -4,3 +4,4 @@ from . import models
 # Register your models here.
 
 admin.site.register(models.Society)
+admin.site.register(models.Profile)
--- api/serializers.py
+++ api/serializers.py
@@ -4,22 +4,28 @@ from . import models
 
 
 
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = models.User
-        fields = ('id','email', 'username', 'first_name', 'last_name', 'age', 'members')
-
-class CreateUserSerializer(serializers.ModelSerializer):
-    class Meta: 
-        model = models.User
-        fields = ('email', 'username', 'first_name', 'last_name', 'age', 'password')
-
 class SocietySerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Society
-        fields = ('id', 'email', 'name', 'description', 'category')
+        fields = ('id', 'email', 'name', 'description', 'category', 'member')
 
 class CreateSocietySerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Society
         fields = ('email', 'name', 'description', 'category')
+
+class CreateUserSerializer(serializers.ModelSerializer):
+
+    age = serializers.CharField(source='profile.age')
+    class Meta: 
+        model = models.User
+        fields = ('email', 'username', 'first_name', 'last_name', 'age', 'password')
+        
+class UserSerializer(serializers.ModelSerializer):
+
+    age = serializers.CharField(source='profile.age')
+    all_society = SocietySerializer(source='profile.members', many=True)
+
+    class Meta:
+        model = models.User
+        fields = ('id','email', 'username', 'first_name', 'last_name', 'age', 'all_society')
