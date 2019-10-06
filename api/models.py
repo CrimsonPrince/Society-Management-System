545d63ec2f7162edfedcb3e3fff1d03f390fd994
crimsonprince <kingr4@protonmail.ch>
2019-10-06 13:14:35 +0100

    Updated UserModel to OneToOne relation

--- api/models.py
+++ api/models.py
@@ -1,5 +1,7 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
 
@@ -9,14 +11,23 @@ class Society(models.Model):
     creation_date = models.DateField(auto_now_add=True)
     description = models.CharField(max_length=1000)
     category = models.CharField(max_length=255)
+    facebook = models.CharField(max_length=1000)
+    instagram = models.CharField(max_length=1000)
     logo = models.CharField(max_length=1024)
 
     def __str__(self):
         return self.name
 
-class User(AbstractUser):
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     age = models.CharField(max_length=4)
     members = models.ManyToManyField('Society', related_name='member')
 
-    def __str__(self):
-        return self.email
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
