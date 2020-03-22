2d209f4f9d46c9acf38c21d91dec215a3b93ca30
crimsonprince <kingr4@protonmail.ch>
2020-03-22 18:07:29 +0000

    Componentized PokmeonList
    Signed-off-by: crimsonprince <kingr4@protonmail.ch>

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,53 +1,14 @@
-import fetch from 'isomorphic-unfetch';
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import GridList from '@material-ui/core/GridList';
-import GridListTile from '@material-ui/core/GridListTile';
-import GridListTileBar from '@material-ui/core/GridListTileBar';
-import ListSubheader from '@material-ui/core/ListSubheader';
-import IconButton from '@material-ui/core/IconButton';
-import InfoIcon from '@material-ui/icons/Info';
-
-const useStyles = makeStyles(theme => ({
-  root: {
-    display: 'flex',
-    flexWrap: 'wrap',
-    justifyContent: 'space-around',
-    overflow: 'hidden',
-    backgroundColor: theme.palette.background.paper,
-  },
-  gridList: {
-    width: "100%",
-    height: "100%",
-    "justify-content": "center",
-  },
-  icon: {
-    color: 'rgba(255, 255, 255, 0.54)',
-  },
-}));
+import Layout from '../components/MyLayout';
+import PokemonList from '../components/PokemonList'
 
 
 export default function index(props) {
-  const classes = useStyles();
 
   return (
-    <div className={classes.root}>
-      <GridList className={classes.gridList} cols={4}>
-        {props.pokemons.map(pokemon => (
-          <GridListTile style={{height: '180px', width: '180px'}} key={pokemon.imageurl}>
-            <img src={pokemon.imageurl} alt={pokemon.name} />
-            <GridListTileBar
-              title={pokemon.name}
-              actionIcon={
-                <IconButton aria-label={`info about ${pokemon.name}`} className={classes.icon}>
-                  <InfoIcon />
-                </IconButton>
-              }
-            />
-          </GridListTile>
-        ))}
-      </GridList>
-    </div>
+    <Layout> 
+    <PokemonList pokemons={props.pokemons}> </PokemonList> 
+    </Layout>
   );
 }
 
