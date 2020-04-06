65be3dce6446ec90f9d0ee0badeb2be700521f52
crimsonprince <kingr4@protonmail.ch>
2020-04-06 20:46:19 +0100

    Added Furhter Styling To Info
    Signed-off-by: crimsonprince <kingr4@protonmail.ch>

--- frontend/src/app/pokemon/pokemon-info/pokemon-info.component.ts
+++ frontend/src/app/pokemon/pokemon-info/pokemon-info.component.ts
@@ -8,17 +8,31 @@ import { Pokemon } from '../pokemon.model';
 @Component({
   selector: 'app-pokemon-info',
   templateUrl: './pokemon-info.component.html',
-  styleUrls: ['./pokemon-info.component.css']
+  styleUrls: ['./pokemon-info.component.scss']
 })
 export class PokemonInfoComponent implements OnInit {
 
   public pokemon: Pokemon;
+  private Color = require('color');
 
   constructor(private route: ActivatedRoute, private pokemonService: PokemonService) { }
 
   ngOnInit(): void {
     this.pokemonService.getPokemon(this.route.snapshot.paramMap.get('id')).subscribe( data => {
       this.pokemon = data;
+      var color = this.Color(this.pokemon.color[0]).darken(0.5);
+      console.log(color.toString());
+      if (this.pokemon.color.length > 1){
+        document.documentElement.style.setProperty('--primary-color', this.pokemon.color[1]);
+        document.documentElement.style.setProperty('--secondary-color', this.pokemon.color[0]);
+        document.documentElement.style.setProperty('--primary-dark', this.Color(this.pokemon.color[1]).darken(0.25));
+      }
+      else {
+        document.documentElement.style.setProperty('--primary-color', this.pokemon.color[0]);
+        document.documentElement.style.setProperty('--secondary-color', this.pokemon.color[0]);
+        document.documentElement.style.setProperty('--primary-dark', this.Color(this.pokemon.color[0]).darken(0.25));
+      }
+      document.documentElement.style.setProperty('--hp', String(255 / this.pokemon.hp) + "%");
     });
   }
 
--- frontend/src/app/pokemon/pokemon-list/pokemon-list.component.ts
+++ frontend/src/app/pokemon/pokemon-list/pokemon-list.component.ts
@@ -5,7 +5,7 @@ import { Pokemon } from '../pokemon.model';
 @Component({
   selector: 'app-pokemon-list',
   templateUrl: './pokemon-list.component.html',
-  styleUrls: ['./pokemon-list.component.css']
+  styleUrls: ['./pokemon-list.component.scss']
 })
 export class PokemonListComponent implements OnInit {
 
